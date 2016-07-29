d1d9e3c168bc904da1cd039a056584085ab50eed
Stephan Hesse <tchakabam@gmail.com>
2016-07-29 15:17:19 +0200

    Custom validation on BaseXHR of properties if need be

--- src/base-xhr.js
+++ src/base-xhr.js
@@ -1,46 +1,62 @@
+/* eslint no-unused-vars: ["error", { "argsIgnorePattern": "^_" }] */
+
 'use strict';
 
 var objectMirrors = require('./object-mirrors');
 
-var BaseXHR = function(xhrImpl) {
+var BaseXHR = function(xhrImpl, defaultValidator) {
 
     var instance = {};
     var xhr = xhrImpl || {};
 
-    objectMirrors.mirrorRwProp(instance, xhr, "responseType");
-    objectMirrors.mirrorRwProp(instance, xhr, "timeout");
-    objectMirrors.mirrorRwProp(instance, xhr, "withCredentials");
+    var defaultGetValidator = function(target, source, prop) {
+        if (defaultValidator) {
+            return defaultValidator(target, source, prop);
+        }
+        return {};
+    };
+    var defaultSetValidator = function(val_, target, source, prop) {
+        if (defaultValidator) {
+            return defaultValidator(target, source, prop);
+        }
+        return {};
+    };
+    var defaultFuncValidator = defaultGetValidator;
 
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "readyState");
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "response");
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "responseText");
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "responseURL");
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "responseXML");
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "status");
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "statusText");
-    objectMirrors.mirrorReadOnlyProp(instance, xhr, "upload");
+    objectMirrors.mirrorRwProp(instance, xhr, "responseType", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "timeout", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "withCredentials", defaultSetValidator, defaultGetValidator);
 
-    objectMirrors.mirrorFunc(instance, xhr, "abort");
-    objectMirrors.mirrorFunc(instance, xhr, "open");
-    objectMirrors.mirrorFunc(instance, xhr, "send");
-    objectMirrors.mirrorFunc(instance, xhr, "setRequestHeader");
-    objectMirrors.mirrorFunc(instance, xhr, "getResponseHeader");
-    objectMirrors.mirrorFunc(instance, xhr, "overrideMimeType");
-    objectMirrors.mirrorFunc(instance, xhr, "getAllResponseHeaders");
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "readyState", defaultGetValidator);
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "response", defaultGetValidator);
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "responseText", defaultGetValidator);
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "responseURL", defaultGetValidator);
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "responseXML", defaultGetValidator);
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "status", defaultGetValidator);
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "statusText", defaultGetValidator);
+    objectMirrors.mirrorReadOnlyProp(instance, xhr, "upload", defaultGetValidator);
+
+    objectMirrors.mirrorFunc(instance, xhr, "abort", defaultFuncValidator);
+    objectMirrors.mirrorFunc(instance, xhr, "open", defaultFuncValidator);
+    objectMirrors.mirrorFunc(instance, xhr, "send", defaultFuncValidator);
+    objectMirrors.mirrorFunc(instance, xhr, "setRequestHeader", defaultFuncValidator);
+    objectMirrors.mirrorFunc(instance, xhr, "getResponseHeader", defaultFuncValidator);
+    objectMirrors.mirrorFunc(instance, xhr, "overrideMimeType", defaultFuncValidator);
+    objectMirrors.mirrorFunc(instance, xhr, "getAllResponseHeaders", defaultFuncValidator);
 
     // EventTarget iface
-    objectMirrors.mirrorFunc(instance, xhr, "addRemoveEventListener");
-    objectMirrors.mirrorFunc(instance, xhr, "removeRemoveEventListener");
+    objectMirrors.mirrorFunc(instance, xhr, "addRemoveEventListener", defaultFuncValidator);
+    objectMirrors.mirrorFunc(instance, xhr, "removeRemoveEventListener", defaultFuncValidator);
 
     // All events as in https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
-    objectMirrors.mirrorRwProp(instance, xhr, "onload");
-    objectMirrors.mirrorRwProp(instance, xhr, "onloadstart");
-    objectMirrors.mirrorRwProp(instance, xhr, "onloadend");
-    objectMirrors.mirrorRwProp(instance, xhr, "onabort");
-    objectMirrors.mirrorRwProp(instance, xhr, "onerror");
-    objectMirrors.mirrorRwProp(instance, xhr, "onprogress");
-    objectMirrors.mirrorRwProp(instance, xhr, "ontimeout");
-    objectMirrors.mirrorRwProp(instance, xhr, "onreadystatechange");
+    objectMirrors.mirrorRwProp(instance, xhr, "onload", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "onloadstart", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "onloadend", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "onabort", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "onerror", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "onprogress", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "ontimeout", defaultSetValidator, defaultGetValidator);
+    objectMirrors.mirrorRwProp(instance, xhr, "onreadystatechange", defaultSetValidator, defaultGetValidator);
 
     return instance;
 };
